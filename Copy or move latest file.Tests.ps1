89534fd8ff8976204fc535956a3aef1e2fe1a650
DarkLite1 <BrechtGijbels@gmail.com>
2021-08-18 16:42:06 +0200

    script born

---
+++ Copy or move latest file.Tests.ps1
@@ -0,0 +1,192 @@
+#Requires -Modules Pester
+#Requires -Version 5.1
+
+BeforeAll {
+    $MailAdminParams = {
+        ($To -eq $ScriptAdmin) -and ($Priority -eq 'High') -and 
+        ($Subject -eq 'FAILURE')
+    }
+
+    $testScript = $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+    $testParams = @{
+        ScriptName          = 'Test (Brecht)'
+        Action              = 'Copy'
+        SourcePath          = (New-Item 'TestDrive:/A' -ItemType Directory).FullName 
+        DestinationPath     = (New-Item 'TestDrive:/B' -ItemType Directory).FullName 
+        DestinationFileName = 'copiedFile.csv'
+        FileExtension       = 'csv'
+        OverWrite           = $true
+        MailTo              = @('bob@contoso.com')
+        LogFolder           = New-Item 'TestDrive:/log' -ItemType Directory
+    }
+
+    Mock Send-MailHC
+    Mock Write-EventLog
+}
+Describe 'the mandatory parameters are' {
+    It '<_>' -ForEach 'ScriptName', 'Action', 'SourcePath', 'DestinationPath' {
+        (Get-Command $testScript).Parameters[$_].Attributes.Mandatory | 
+        Should -BeTrue
+    }
+}
+Describe 'throw a terminating error when' {
+    It 'the log folder cannot be created' {
+        $testNewParams = $testParams.clone()
+        $testNewParams.LogFolder = 'xx:://x'
+
+        { .$testScript @testNewParams } | Should -Throw "Failed creating the log folder 'xx:://x'*"
+    }
+    It 'the source folder cannot be found' {
+        $testNewParams = $testParams.clone()
+        $testNewParams.SourcePath = 'TestDrive:/x'
+
+        { .$testScript @testNewParams } | Should -Throw "Failed checking the source folder '*/x'*"
+    } 
+}
+Describe 'copy only the latest file found in the source folder' {
+    BeforeAll {
+        $testSourceFiles = @(
+            '1.txt', '2.txt', '3.txt', 
+            '1.csv', '2.csv', 
+            '1.xlsx'
+        ) | ForEach-Object {
+            (New-Item (Join-Path $testParams.SourcePath $_) -ItemType File).FullName
+        }
+    }
+    BeforeEach {
+        Get-ChildItem $testParams.DestinationPath | Remove-Item
+    }
+    It 'regardless its extension' {
+        $testNewParams = $testParams.clone()
+        $testNewParams.Remove('FileExtension')
+        $testNewParams.Remove('DestinationFileName')
+        . $testScript @testNewParams
+        $actual = Get-ChildItem $testParams.DestinationPath
+        $actual | Should -HaveCount 1
+        $actual.Name | Should -Be '1.xlsx'
+    }
+    It 'regardless its extension with a new name' {
+        $testNewParams = $testParams.clone()
+        $testNewParams.Remove('FileExtension')
+        $testNewParams.DestinationFileName = 'A'
+        . $testScript @testNewParams
+        $actual = Get-ChildItem $testParams.DestinationPath
+        $actual | Should -HaveCount 1
+        $actual.Name | Should -Be 'A.xlsx'
+    }
+    It 'for a specific extension' {
+        $testNewParams = $testParams.clone()
+        $testNewParams.FileExtension = '.csv'
+        $testNewParams.Remove('DestinationFileName')
+        . $testScript @testNewParams
+        $actual = Get-ChildItem $testParams.DestinationPath
+        $actual | Should -HaveCount 1
+        $actual.Name | Should -Be '2.csv'
+    }
+    It 'for a specific extension with a new name' {
+        $testNewParams = $testParams.clone()
+        $testNewParams.FileExtension = '.csv'
+        $testNewParams.DestinationFileName = 'A'
+        . $testScript @testNewParams
+        $actual = Get-ChildItem $testParams.DestinationPath
+        $actual | Should -HaveCount 1
+        $actual.Name | Should -Be 'A.csv'
+    }
+    It "and remove the source file when action is 'Move'" {
+        $testNewParams = $testParams.clone()
+        $testNewParams.FileExtension = '.csv'
+        $testNewParams.DestinationFileName = 'A'
+        $testNewParams.Action = 'Move'
+        . $testScript @testNewParams
+        
+        $testSourceFiles = @(
+            '1.txt', '2.txt', '3.txt', 
+            '1.csv', # '2.csv', 
+            '1.xlsx'
+        ) | ForEach-Object {
+            Join-Path $testParams.SourcePath $_ | Should -Exist
+        }
+        Join-Path $testParams.SourcePath '2.csv' | Should -Not -Exist
+    } 
+}
+Describe 'when the file name already exists in the destination folder' {
+    BeforeAll {
+        $testSourceFile = New-Item (
+            Join-Path $testParams.SourcePath '1.txt') -ItemType File
+        'A' | Out-File -LiteralPath  $testSourceFile
+    }
+    BeforeEach {
+        $testDestinationFile = New-Item (
+            Join-Path $testParams.DestinationPath '1.txt') -ItemType File -Force
+        'B' | Out-File -LiteralPath  $testDestinationFile
+    }
+    It 'it is over written when OverWrite is true' {
+        $testNewParams = $testParams.clone()
+        $testNewParams.Remove('FileExtension')
+        $testNewParams.Remove('DestinationFileName')
+        $testNewParams.Remove('OverWrite')
+        . $testScript @testNewParams -OverWrite
+        $actual = Get-ChildItem $testParams.DestinationPath
+        Get-Content -Path $actual.FullName | Should -BeExactly 'A'
+    } 
+    It 'it is not over written when OverWrite is false and an error is thrown' {
+        $testNewParams = $testParams.clone()
+        $testNewParams.Remove('FileExtension')
+        $testNewParams.Remove('DestinationFileName')
+        $testNewParams.Remove('OverWrite')
+        { . $testScript @testNewParams } | 
+        Should -Throw "The file '$($testDestinationFile.FullName)' already exists in the destination folder, use 'OverWrite'*"
+        $actual = Get-ChildItem $testParams.DestinationPath
+        Get-Content -Path $actual.FullName | Should -BeExactly 'B'
+    }
+}
+Describe 'send a summary mail' {
+    It 'to users and the admin when MailTo is used' {
+        New-Item (Join-Path $testParams.SourcePath '1.csv') -ItemType File
+
+        $testNewParams = $testParams.clone()
+        $testNewParams.FileExtension = '.csv'
+        $testNewParams.DestinationFileName = 'A'
+        $testNewParams.Action = 'Move'
+        $testNewParams.MailTo = 'bob@contoso.com'
+        . $testScript @testNewParams
+    
+        Should -Invoke Send-MailHC -Exactly 1 -ParameterFilter {
+            ($To -eq 'bob@contoso.com') -and
+            ($Bcc -eq $ScriptAdmin) -and
+            ($Priority -eq 'Normal') -and
+            ($Subject -eq 'File moved') -and
+            ($Message -like "*<b>Move</b> the most recently edited file with <b>extension '.csv'</b> from the <a href=`"$($testNewParams.SourcePath)`">source folder</a> to the <a href=`"$($testNewParams.DestinationPath)`">destination folder</a> and <b>over write the destination file</b> when it exists already.*
+        *<th>Destination file name</th>*
+        *<td><a href=`"$($testNewParams.DestinationPath + '\A.csv')`">$($testNewParams.DestinationPath + '\A.csv')</a></td>*
+        *<th>Source file name</th>*
+        *<td><a href=`"$($testNewParams.SourcePath + '\1.csv')`">$($testNewParams.SourcePath + '\1.csv')</a></td>*
+        *<th>Source file LastWriteTime</th>*"
+            )
+        }
+    }
+    It 'only to the admin when MailTo is not used' {
+        New-Item (Join-Path $testParams.SourcePath '1.csv') -ItemType File
+
+        $testNewParams = $testParams.clone()
+        $testNewParams.FileExtension = '.csv'
+        $testNewParams.DestinationFileName = 'A'
+        $testNewParams.Action = 'Move'
+        $testNewParams.Remove('MailTo')
+        . $testScript @testNewParams
+    
+        Should -Invoke Send-MailHC -Exactly 1 -ParameterFilter {
+            ($To -eq $ScriptAdmin) -and
+            (-not $Bcc) -and
+            ($Priority -eq 'Normal') -and
+            ($Subject -eq 'File moved') -and
+            ($Message -like "*<b>Move</b> the most recently edited file with <b>extension '.csv'</b> from the <a href=`"$($testNewParams.SourcePath)`">source folder</a> to the <a href=`"$($testNewParams.DestinationPath)`">destination folder</a> and <b>over write the destination file</b> when it exists already.*
+        *<th>Destination file name</th>*
+        *<td><a href=`"$($testNewParams.DestinationPath + '\A.csv')`">$($testNewParams.DestinationPath + '\A.csv')</a></td>*
+        *<th>Source file name</th>*
+        *<td><a href=`"$($testNewParams.SourcePath + '\1.csv')`">$($testNewParams.SourcePath + '\1.csv')</a></td>*
+        *<th>Source file LastWriteTime</th>*"
+            )
+        }
+    }
+}
---
+++ Copy or move latest file.ps1
@@ -0,0 +1,288 @@
+<# 
+    .SYNOPSIS   
+        Copy or move the most recently edited file to another folder.
+
+    .DESCRIPTION
+        Copy or move the most recently edited file found in the source folder 
+        to the destination folder. When FileExtension is given, only files 
+        matching the extension will be searched. When DestinationFileName is 
+        given, the file will be renamed in the destination folder.
+
+    .PARAMETER Action
+        When set to 'Copy' the source file will be left alone and when set to
+        'Move' the source file will be deleted after copying.
+
+    .PARAMETER SourcePath
+        Folder where to search for the most recently edited file.
+
+    .PARAMETER DestinationPath
+        Folder to where to copy the most recently edited file.
+
+    .PARAMETER FileExtension
+        The file extension to look for. If none is given the latest file
+        is selected regardless of extension (Ex. '.csv').
+
+    .PARAMETER DestinationFileName
+        New name for the copied file without its extension (Ex. 'Copied').
+
+    .PARAMETER OverWrite
+        If the file is already there it will be overwritten when OverWrite is
+        set to true. When OverWrite is set to false an error email will be sent
+        to the users in MailTo.
+
+    .PARAMETER MailTo
+        Inform users by sending an email after running the script on success 
+        and failure. When MailTo is empty no email will be sent.
+
+    .EXAMPLE
+        $params = @{
+            ScriptName      = 'Copy latest file'
+            Action          = 'Copy'
+            SourcePath      = 'C:\FolderA'
+            DestinationPath = 'C:\FolderB'
+        }
+        . $scriptPath @params
+
+        Copy the most recent file from FolderA to FolderB
+
+    .EXAMPLE
+        $params = @{
+            ScriptName      = 'Move latest txt file'
+            Action          = 'Move'
+            SourcePath      = 'C:\FolderA'
+            DestinationPath = 'C:\FolderB'
+            FileExtension   = '.txt'
+            OverWrite       = $true
+        }
+        . $scriptPath @params
+
+        Move the most recent file from FolderA to FolderB that has file 
+        extension '.txt' and over write the file in the destination when it 
+        exists already
+
+    .EXAMPLE
+        $params = @{
+            ScriptName          = 'Copy latest csv file'
+            Action              = 'Copy'
+            SourcePath          = 'C:\FolderA'
+            DestinationPath     = 'C:\FolderB'
+            FileExtension       = '.csv'
+            DestinationFileName = 'copied.txt'
+        }
+        . $scriptPath @params
+
+        Copy the most recent file from FolderA to FolderB that has file 
+        extension '.csv' and rename the file in the destination folder to 
+        'copied.txt'. Because 'OverWrite' is not used an existing file on the 
+        destination will not be over written. You can however add this switch 
+        if you want that. 
+#>
+                
+Param (
+    [Parameter(Mandatory)]
+    [String]$ScriptName,
+    [Parameter(Mandatory)]
+    [String]$SourcePath,
+    [Parameter(Mandatory)]
+    [String]$DestinationPath,
+    [Parameter(Mandatory)]
+    [ValidateSet('Copy', 'Move')]
+    [String]$Action,
+    [String]$DestinationFileName,
+    [String]$FileExtension,
+    [Switch]$OverWrite,
+    [String[]]$MailTo,
+    [String]$LogFolder = $env:POWERSHELL_LOG_FOLDER,
+    [String]$ScriptAdmin = $env:POWERSHELL_SCRIPT_ADMIN
+)
+
+Begin {
+    Try {
+        Import-EventLogParamsHC -Source $ScriptName
+        Write-EventLog @EventStartParams
+        Get-ScriptRuntimeHC -Start
+        $Error.Clear()
+
+        #region Logging
+        try {
+            $LogParams = @{
+                LogFolder    = New-Item -Path "$LogFolder\File and folder" -ItemType 'Directory' -Force -ErrorAction 'Stop'
+                Name         = $ScriptName
+                Date         = 'ScriptStartTime'
+                NoFormatting = $true
+            }
+            $LogFile = New-LogFileNameHC @LogParams
+        }
+        Catch {
+            throw "Failed creating the log folder '$LogFolder': $_"
+        }
+        #endregion
+
+        $mailParams = @{}
+    }
+    Catch {
+        Write-Warning $_
+        Write-EventLog @EventErrorParams -Message "FAILURE:`n`n- $_"
+        Write-EventLog @EventEndParams
+        $errorMessage = $_; $global:error.RemoveAt(0); throw $errorMessage
+    }
+}
+
+Process {
+    Try {
+        #region Create source search parameters
+        $getParams = @{
+            LiteralPath = $SourcePath
+            File        = $true
+            Force       = $true
+            ErrorAction = 'Stop'
+        }
+        if ($FileExtension) {
+            $getParams.Filter = '*{0}' -f $FileExtension
+        }
+        #endregion
+
+        #region Get latest source file
+        Try {
+            $fileToMove = Get-ChildItem @getParams | 
+            Sort-Object LastWriteTime | Select-Object -Last 1
+        }
+        Catch {
+            throw "Failed checking the source folder '$SourcePath': $_"
+        }
+        #endregion
+
+        if ($fileToMove) {
+            #region Create destination copy parameters
+            $joinParam = @{
+                Path      = $DestinationPath
+                ChildPath = if ($DestinationFileName) {
+                    $DestinationFileName + $fileToMove.Extension
+                }
+                else { $fileToMove.Name }
+            }
+            $copyParams = @{
+                Path        = $fileToMove.FullName
+                Destination = Join-Path @joinParam
+                Force       = $true
+                ErrorAction = 'Stop'
+            }
+            #endregion
+
+            #region Test if destination file already exists
+            if (
+                (-not $OverWrite) -and 
+                (Test-Path -LiteralPath $copyParams.Destination -PathType Leaf)
+            ) {
+                throw "The file '$($copyParams.Destination)' already exists in the destination folder, use 'OverWrite' if you want to over write this file."
+            }
+            #endregion
+
+            #region Copy source file to destination folder
+            Try {
+                Copy-Item @copyParams 
+            }
+            Catch {
+                throw "Failed to copy file '$($fileToMove.FullName)' to the destination folder '$DestinationPath': $_"
+            }
+            #endregion
+
+            #region Remove source file
+            if (
+                ($Action -eq 'Move') -and 
+                (Test-Path -LiteralPath $fileToMove.FullName -PathType Leaf)
+            ) {
+                Remove-Item -LiteralPath $fileToMove.FullName -Force
+            }
+            #endregion
+        }
+        else {
+            Write-Warning "No file found in source folder '$SourcePath'"
+        }
+    }
+    Catch {
+        Write-Warning $_
+        Write-EventLog @EventErrorParams -Message "FAILURE:`n`n- $_"
+        Write-EventLog @EventEndParams
+        $errorMessage = $_; $global:error.RemoveAt(0); throw $errorMessage
+    }
+}
+End {
+    Try {
+        #region Send email
+        $mailParams.Message = (
+            $( 
+                '<p>'
+                $(if ($Action = 'Move') {
+                        '<b>Move</b> the most recently edited file'
+                    }
+                    else {
+                        '<b>Copy</b> the most recently edited file'
+                    })
+                $(if ($FileExtension) { 
+                        " with <b>extension '$FileExtension'</b>" 
+                    }
+                )
+                $(" from the <a href=`"$SourcePath`">source folder</a> to the <a href=`"$DestinationPath`">destination folder</a>")
+                $(if ($OverWrite) { 
+                        " and <b>over write the destination file</b> when it exists already" 
+                    }
+                )
+                '.<p>'
+            ) | Where-Object { $_ }
+        ) -join ''
+        
+        if ($fileToMove) {
+            $mailParams.Message += "
+                <p>File details:</p>
+                <table>
+                    <tr>
+                        <th>Destination file name</th>
+                        <td><a href=`"$($copyParams.Destination)`">$($copyParams.Destination)</a></td>
+                    </tr>
+                    <tr>
+                        <th>Source file name</th>
+                        <td><a href=`"$($fileToMove.FullName)`">$($fileToMove.FullName)</a></td>
+                    </tr>
+                    <tr>
+                        <th>Source file LastWriteTime</th>
+                        <td>$(($fileToMove.LastWriteTime).ToString('dd/MM/yyyy HH:mm:ss'))</td>
+                    </tr>
+                </table>
+            "
+            $mailParams.subject = 'File {0}' -f $(
+                if ($Action = 'Move') { 'moved' } else { 'copied' }
+            )
+            $mailParams.Priority = 'Normal'
+        }
+        else {
+            $mailParams.Message += '<p>No file found in the source folder matching the search criteria.</p>'
+            $mailParams.subject = 'No file {0}' -f $(
+                if ($Action = 'Move') { 'moved' } else { 'copied' }
+            )
+            $mailParams.Priority = 'High'
+        }
+
+
+        $mailParams += @{
+            To        = $ScriptAdmin
+            LogFolder = $LogParams.LogFolder
+            Header    = $ScriptName
+            Save      = $LogFile + ' - Mail.html'
+        }
+        if ($MailTo) {
+            $mailParams.To = $MailTo
+            $mailParams.Bcc = $ScriptAdmin
+        }
+               
+        Get-ScriptRuntimeHC -Stop
+        Send-MailHC @mailParams
+        #endregion
+    }
+    Catch {
+        Write-Warning $_
+        Write-EventLog @EventErrorParams -Message "FAILURE:`n`n- $_"
+        Write-EventLog @EventEndParams
+        $errorMessage = $_; $global:error.RemoveAt(0); throw $errorMessage
+    }
+}
