6ab73d54195a334ed5cde5ad0a1b7e7332d0bc7b
DarkLite1 <BrechtGijbels@gmail.com>
2024-01-15 13:24:06 +0100

    update Catch clause

--- Copy or move latest file.ps1
+++ Copy or move latest file.ps1
@@ -1,15 +1,15 @@
 #Requires -Version 5.1
 #Requires -Modules Toolbox.EventLog, Toolbox.HTML
 
-<# 
-    .SYNOPSIS   
+<#
+    .SYNOPSIS
         Copy or move the most recently edited file to another folder.
 
     .DESCRIPTION
-        Copy or move the most recently edited file found in the source folder 
-        to the destination folder. When 'FileExtension' is given, only files 
-        matching the extension will be searched. When 'DestinationFileName' is 
-        given, the file will be renamed in the destination folder. When 
+        Copy or move the most recently edited file found in the source folder
+        to the destination folder. When 'FileExtension' is given, only files
+        matching the extension will be searched. When 'DestinationFileName' is
+        given, the file will be renamed in the destination folder. When
         'FileNameStartsWith' is given, only files that start with that specific
         string are checked in the source folder.
 
@@ -24,11 +24,11 @@
         Folder to where to copy the most recently edited file.
 
     .PARAMETER FileExtension
-        The file extension to look for in the source folder. If none is given 
+        The file extension to look for in the source folder. If none is given
         the latest file is selected regardless of extension (Ex. '.csv').
 
     .PARAMETER FileNameStartsWith
-        Only files in the source folder where the file name starts with a 
+        Only files in the source folder where the file name starts with a
         specific string will be searched, other files will be excluded.
 
     .PARAMETER DestinationFileName
@@ -40,12 +40,12 @@
         to the users in MailTo.
 
     .PARAMETER MailTo
-        Inform users by sending an email after running the script on success 
+        Inform users by sending an email after running the script on success
         and failure. When MailTo is empty no email will be sent.
 
     .NOTES
-        A copied file can be renamed in the destination folder but its 
-        extension cannot be renamed. Renaming an extension in the destination 
+        A copied file can be renamed in the destination folder but its
+        extension cannot be renamed. Renaming an extension in the destination
         folder is not supported.
 
     .EXAMPLE
@@ -59,9 +59,9 @@
         . $scriptPath @params
 
         Copy the most recent file from FolderA to FolderB, regardless its file
-        extension or file name, and send a summary mail to Chuck Norris. 
-        
-        In case the file is already in the destination folder it will not be 
+        extension or file name, and send a summary mail to Chuck Norris.
+
+        In case the file is already in the destination folder it will not be
         overwritten because 'OverWrite' is not used.
 
     .EXAMPLE
@@ -75,10 +75,10 @@
         }
         . $scriptPath @params
 
-        Move the most recent file from FolderA to FolderB that has file 
-        extension '.txt', regardless of its name, and overwrite the file in the 
-        destination folder when it already exists. 
-        
+        Move the most recent file from FolderA to FolderB that has file
+        extension '.txt', regardless of its name, and overwrite the file in the
+        destination folder when it already exists.
+
         No mail is sent because 'MailTo' is not used.
 
     .EXAMPLE
@@ -92,11 +92,11 @@
         }
         . $scriptPath @params
 
-        Copy the most recent file from FolderA to FolderB that has file 
-        extension '.csv' and rename the file in the destination folder to 
-        'copied.csv'. 
-        
-        Because 'OverWrite' is not used an existing file in the destination 
+        Copy the most recent file from FolderA to FolderB that has file
+        extension '.csv' and rename the file in the destination folder to
+        'copied.csv'.
+
+        Because 'OverWrite' is not used an existing file in the destination
         folder will not be overwritten. No mails are sent because 'MailTo'
         is not used either.
 
@@ -114,12 +114,12 @@
         }
         . $scriptPath @params
 
-        Copy the most recent file from FolderA to FolderB that has file 
+        Copy the most recent file from FolderA to FolderB that has file
         extension '.zip' and a file name that starts with 'backup'. Rename
-        the file in the destination folder to 'latest backup.zip' and overwrite 
+        the file in the destination folder to 'latest backup.zip' and overwrite
         the file when it already exists. Send a summary mail to Chuck Norris.
 #>
-                
+
 Param (
     [Parameter(Mandatory)]
     [String]$ScriptName,
@@ -168,9 +168,9 @@ Begin {
     }
     Catch {
         Write-Warning $_
+        Send-MailHC -To $ScriptAdmin -Subject 'FAILURE' -Priority 'High' -Message $_ -Header $ScriptName
         Write-EventLog @EventErrorParams -Message "FAILURE:`n`n- $_"
-        Write-EventLog @EventEndParams
-        $errorMessage = $_; $global:error.RemoveAt(0); throw $errorMessage
+        Write-EventLog @EventEndParams; Exit 1
     }
 }
 
@@ -185,7 +185,7 @@ Process {
                 Force       = $true
                 ErrorAction = 'Stop'
             }
-            $fileToMove = Get-ChildItem @getParams | 
+            $fileToMove = Get-ChildItem @getParams |
             Sort-Object LastWriteTime | Select-Object -Last 1
         }
         Catch {
@@ -212,7 +212,7 @@ Process {
 
             #region Test if destination file already exists
             if (
-                (-not $OverWrite) -and 
+                (-not $OverWrite) -and
                 (Test-Path -LiteralPath $copyParams.Destination -PathType Leaf)
             ) {
                 throw "The file '$($copyParams.Destination)' already exists in the destination folder, use 'OverWrite' if you want to over write this file."
@@ -221,7 +221,7 @@ Process {
 
             #region Copy source file to destination folder
             Try {
-                Copy-Item @copyParams 
+                Copy-Item @copyParams
             }
             Catch {
                 throw "Failed to copy file '$($fileToMove.FullName)' to the destination folder '$DestinationFolder': $_"
@@ -230,7 +230,7 @@ Process {
 
             #region Remove source file
             if (
-                ($Action -eq 'Move') -and 
+                ($Action -eq 'Move') -and
                 (Test-Path -LiteralPath $fileToMove.FullName -PathType Leaf)
             ) {
                 Remove-Item -LiteralPath $fileToMove.FullName -Force
@@ -243,16 +243,16 @@ Process {
     }
     Catch {
         Write-Warning $_
+        Send-MailHC -To $ScriptAdmin -Subject 'FAILURE' -Priority 'High' -Message $_ -Header $ScriptName
         Write-EventLog @EventErrorParams -Message "FAILURE:`n`n- $_"
-        Write-EventLog @EventEndParams
-        $errorMessage = $_; $global:error.RemoveAt(0); throw $errorMessage
+        Write-EventLog @EventEndParams; Exit 1
     }
 }
 End {
     Try {
         #region Send email
         $mailParams.Message = (
-            $( 
+            $(
                 '<p>'
                 $(if ($Action -eq 'Move') {
                         '<b>Move</b> the most recently edited file'
@@ -260,23 +260,23 @@ End {
                     else {
                         '<b>Copy</b> the most recently edited file'
                     })
-                $(if ($FileExtension) { 
-                        " with <b>extension '$FileExtension'</b>" 
+                $(if ($FileExtension) {
+                        " with <b>extension '$FileExtension'</b>"
                     }
                 )
-                $(if ($FileNameStartsWith) { 
-                        " where the file name <b>starts with '$FileNameStartsWith'</b>" 
+                $(if ($FileNameStartsWith) {
+                        " where the file name <b>starts with '$FileNameStartsWith'</b>"
                     }
                 )
                 $(" from the <a href=`"$SourceFolder`">source folder</a> to the <a href=`"$DestinationFolder`">destination folder</a>")
-                $(if ($OverWrite) { 
-                        " and <b>overwrite</b> the destination file when it exists already" 
+                $(if ($OverWrite) {
+                        " and <b>overwrite</b> the destination file when it exists already"
                     }
                 )
                 '.<p>'
             ) | Where-Object { $_ }
         ) -join ''
-        
+
         if ($fileToMove) {
             $mailParams.Message += "
                 <p>$(if ($Action -eq 'Move') { 'Moved' } else { 'Copied' }) file:</p>
@@ -318,15 +318,18 @@ End {
             $mailParams.To = $MailTo
             $mailParams.Bcc = $ScriptAdmin
         }
-               
+
         Get-ScriptRuntimeHC -Stop
         Send-MailHC @mailParams
         #endregion
     }
-    Catch {
+    catch {
         Write-Warning $_
+        Send-MailHC -To $ScriptAdmin -Subject 'FAILURE' -Priority 'High' -Message $_ -Header $ScriptName
         Write-EventLog @EventErrorParams -Message "FAILURE:`n`n- $_"
+        Exit 1
+    }
+    Finally {
         Write-EventLog @EventEndParams
-        $errorMessage = $_; $global:error.RemoveAt(0); throw $errorMessage
     }
 }
